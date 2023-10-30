3779ae368b9a80c31ff74844d505bb1fdcb0025e
seo <seowlfh@laposte.net>
2023-10-30 23:33:32 +0100

    feat: cool stuff

--- init.lua
+++ init.lua
@@ -60,11 +60,11 @@ vim.g.mapleader = ' '
 
 -- Kitty autocommands to change padding/background when entering Neovim
 local kitty_mp = vim.api.nvim_create_augroup("kitty_mp", { clear = true })
-
+ 
 vim.api.nvim_create_autocmd({ "VimEnter" }, { pattern = { "*" }, command = "sil !kitty @ --to=$KITTY_LISTEN_ON set-background-opacity 1.0 ", group = kitty_mp})
 vim.api.nvim_create_autocmd({ "VimEnter" }, { pattern = { "*" }, command = "sil !kitty @ --to=$KITTY_LISTEN_ON set-spacing padding=0 margin=0", group = kitty_mp})
 vim.api.nvim_create_autocmd({ "VimEnter" }, { pattern = { "*" }, command = "sil !kitty @ --to=$KITTY_LISTEN_ON set-colors background=\\#1F2430", group = kitty_mp})
-
+ 
 vim.api.nvim_create_autocmd({ "VimLeave" }, { pattern = { "*" }, command = "sil !kitty @ --to=$KITTY_LISTEN_ON set-spacing padding=default margin=default", group = kitty_mp })
 vim.api.nvim_create_autocmd({ "VimLeave" }, { pattern = { "*" }, command = "sil !kitty @ --to=$KITTY_LISTEN_ON set-colors ~/.config/kitty/current-theme.conf", group = kitty_mp})
 vim.api.nvim_create_autocmd({ "VimLeave" }, { pattern = { "*" }, command = "sil !kitty @ --to=$KITTY_LISTEN_ON set-background-opacity 0.8 ", group = kitty_mp})

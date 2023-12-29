922680b747dbced26fd5685b55750b228d47ff4f
seo <seowlfh@laposte.net>
2023-12-29 21:44:04 +0100

    feat: formatter now restore cursor position

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,9 +1,9 @@
-vim.o.shiftwidth = 2
+vim.o.shiftwidth = 4
 
-vim.bo.formatprg = "nixpkgs-fmt"
+vim.bo.formatprg = "stylua --search-parent-directories - 2>/dev/null"
 
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-    pattern = { "*.nix" },
+    pattern = { "*.lua" },
     callback = function(ev)
         local line, col = unpack(vim.api.nvim_win_get_cursor(0))
         vim.cmd.normal("gggqG")
--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -20,44 +20,78 @@ local servers = {
     clangd = {},
     cmake = {},
     rust_analyzer = {
-        ['rust-analyzer'] = {
-            diagnostics = {
-                enable = false;
-            }
+        settings = {
+            ["rust-analyzer"] = {
+                diagnostics = {
+                    enable = false,
+                },
+            },
         },
     },
     nil_ls = {},
-    lua_ls = {},
+    lua_ls = {
+        on_init = function(client)
+            local path = client.workspace_folders[1].name
+            if not vim.loop.fs_stat(path .. "/.luarc.json") and not vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+                client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
+                    Lua = {
+                        runtime = {
+                            -- Tell the language server which version of Lua you're using
+                            -- (most likely LuaJIT in the case of Neovim)
+                            version = "LuaJIT",
+                        },
+                        -- Make the server aware of Neovim runtime files
+                        workspace = {
+                            checkThirdParty = false,
+                            library = {
+                                vim.env.VIMRUNTIME,
+                                -- "${3rd}/luv/library"
+                                -- "${3rd}/busted/library",
+                            },
+                            -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+                            -- library = vim.api.nvim_get_runtime_file("", true)
+                        },
+                    },
+                })
+
+                client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+            end
+            return true
+        end,
+    },
     texlab = {},
     ocamllsp = {
         settings = {
             codelens = { enable = true },
             extendedHover = { enable = true },
-        }
+        },
     },
     hls = {
-        filetypes = { 'haskell', 'lhaskell', 'cabal' },
+        settings = {
+            filetypes = { "haskell", "lhaskell", "cabal" },
+        },
     },
 }
 
-local lspconfig = require('lspconfig')
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local lspconfig = require("lspconfig")
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 for server, config in pairs(servers) do
-    lspconfig[server].setup {
-        on_attach = on_attach,
-        settings = config,
-        capabilities = capabilities
-    }
+    lspconfig[server].setup({
+        on_attach = config.on_attach,
+        settings = config.settings,
+        capabilities = config.capabilities,
+        on_init = config.on_init,
+    })
 end
 
 -- Set up nvim-cmp.
-local cmp = require'cmp'
+local cmp = require("cmp")
 
 cmp.setup({
     snippet = {
         expand = function(args)
-            require('luasnip').lsp_expand(args.body)
+            require("luasnip").lsp_expand(args.body)
         end,
     },
     window = {
@@ -65,26 +99,25 @@ cmp.setup({
         -- documentation = cmp.config.window.bordered(),
     },
     mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
     sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
     }, {
-        { name = 'buffer' },
-    })
+        { name = "buffer" },
+    }),
 })
 
 -- Set configuration for specific filetype.
-cmp.setup.filetype('gitcommit', {
+cmp.setup.filetype("gitcommit", {
     sources = cmp.config.sources({
-        { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+        { name = "git" }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
     }, {
-        { name = 'buffer' },
-    })
+        { name = "buffer" },
+    }),
 })
-

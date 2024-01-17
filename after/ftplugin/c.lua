8f7713cfba6e09d2e530df917f8fa9e04742aa41
seo <seowlfh@laposte.net>
2024-01-17 13:47:19 +0100

    feat(formatter): c/cpp don't spam autocmd

--- after/ftplugin/c.lua
+++ after/ftplugin/c.lua
@@ -1,10 +1,10 @@
 vim.bo.formatprg = "clang-format"
 
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-    pattern = { "*.c", "*.h" },
-    callback = function(ev)
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        vim.cmd.normal("gggqG")
-        vim.api.nvim_win_set_cursor(0, { line, col })
-    end,
-})
+-- vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+--     pattern = { "*.c", "*.h" },
+--     callback = function(_)
+--         local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+--         vim.cmd.normal("gggqG")
+--         vim.api.nvim_win_set_cursor(0, { line, col })
+--     end,
+-- })
--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -4,52 +4,65 @@ return {
         "kylechui/nvim-surround",
     },
     {
-        'jinh0/eyeliner.nvim',
+        "jinh0/eyeliner.nvim",
         config = function()
             -- vim.api.nvim_create_autocmd({'FileType'}, {
             --     pattern = 'NeogitStatus',
             --     callback = function()
             --         cmd ':EyelinerDisable'
             --     end
-            -- }) 
-            require('eyeliner').setup({
+            -- })
+            require("eyeliner").setup({
                 highlight_on_key = true,
                 dim = true,
             })
-        end
-    },
-    {
-        'windwp/nvim-autopairs',
-        config = function()
-            require("nvim-autopairs").setup()
-        end
-    },
-    {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.5', -- or                            , branch = '0.1.x',
-        dependencies = { {'nvim-lua/plenary.nvim'} },
-        config = function()
-            local builtin = require('telescope.builtin')
-
-            vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-            vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-            vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-            vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
-            vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, {})
-            vim.keymap.set('n', '<leader>fw', builtin.lsp_workspace_symbols, {})
         end,
     },
     {
-        'numToStr/Comment.nvim',
+        "windwp/nvim-autopairs",
         config = function()
-            require('Comment').setup()
-        end
+            require("nvim-autopairs").setup()
+        end,
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.5", -- or                            , branch = '0.1.x',
+        dependencies = { { "nvim-lua/plenary.nvim" } },
+        config = function()
+            local builtin = require("telescope.builtin")
+
+            vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+            vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+            vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+            vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+            vim.keymap.set("n", "<leader>fd", builtin.diagnostics, {})
+            vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, {})
+            vim.keymap.set("n", "<leader>fw", builtin.lsp_workspace_symbols, {})
+        end,
+    },
+    {
+        "numToStr/Comment.nvim",
+        config = function()
+            require("Comment").setup()
+        end,
     },
     -- Autocomplete
-    {'neovim/nvim-lspconfig'},
-    {'hrsh7th/nvim-cmp'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/cmp-path'},
-    {'hrsh7th/cmp-buffer'}
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "folke/neodev.nvim",
+            dependencies = { "hrsh7th/nvim-cmp" },
+            opts = {},
+            config = function(opts)
+                require("neodev").setup(opts)
+            end,
+        },
+        config = function()
+            require("lsp")
+        end,
+    },
+    { "hrsh7th/nvim-cmp" },
+    { "hrsh7th/cmp-nvim-lsp" },
+    { "hrsh7th/cmp-path" },
+    { "hrsh7th/cmp-buffer" },
 }

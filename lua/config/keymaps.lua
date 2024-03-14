8ab1003e3c507c4a65d58932c5fa996f9b286489
Theo Gardet <theo.gardet@epita.fr>
2024-03-14 11:19:44 +0100

    feat: added toggleterm + QoL maps/autocmd

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,18 @@
 -- Keybinds
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
-vim.keymap.set('n', '<leader>ot', ':NvimTreeToggle <CR>', {silent = true, noremap = true})
+-- Easier movement accros  buffers
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+
+-- Move through tabs
+vim.keymap.set("n", "<S-h>", "<Cmd>tabprevious<CR>")
+vim.keymap.set("n", "<S-l>", "<Cmd>tabnext<CR>")
+
+-- NvimTree
+vim.keymap.set("n", "<leader>ot", ":NvimTreeToggle <CR>", { silent = true, noremap = true })
+
+-- Toggleterm
+vim.keymap.set("t", "<ESC>", [[<C-\><C-n>]])

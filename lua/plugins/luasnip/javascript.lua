2d017445f2c9443b513e550a430303a6656d114f
seo <seowlfh@laposte.net>
2024-04-04 12:33:17 +0200

    feat: nvim-dap, which-key, snippets for js/md, added desc to harpoon keys

---
+++ lua/plugins/luasnip/javascript.lua
@@ -0,0 +1,34 @@
+local ls = require("luasnip")
+
+local s = ls.s
+local i = ls.insert_node
+local rep = require("luasnip.extras").rep
+local fmt = require("luasnip.extras.fmt").fmt
+
+return {
+    s(
+        "index",
+        fmt(
+            [[
+        const {{ {} }} = require("./{}");
+
+        {}();
+    ]],
+            { i(1, "packageName"), rep(1), rep(1) }
+        )
+    ),
+    s(
+        "module",
+        fmt(
+            [[
+        function {}({}) {{
+            {}
+        }}
+
+        modules.export = {{
+            {},
+        }}]],
+            { i(1, "funcName"), i(2, "params"), i(3), rep(1) }
+        )
+    ),
+}

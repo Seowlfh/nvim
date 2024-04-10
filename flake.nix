fe04474bd076d80731fa1098ed2209f1e75faa9c
Theo GARDET <theo.gardet@epita.fr>
2024-04-10 23:45:18 +0200

    knock knock it's the united flakes

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell
+        {
+          buildInputs = with pkgs; [
+            # Fill stuff here
+            stylua
+            lua-language-server
+          ];
+        };
+    };
+}

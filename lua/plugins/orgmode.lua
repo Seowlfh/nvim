c79ed235204c889a3776831ccc1fcb774c04cbfb
seo <seowlfh@laposte.net>
2024-10-14 17:19:15 +0200

    feat(orgmode): add org-mode-bullets, and move org mode into a file

---
+++ lua/plugins/orgmode.lua
@@ -0,0 +1,29 @@
+return {
+    {
+        'nvim-orgmode/orgmode',
+        event = 'VeryLazy',
+        ft = { 'org' },
+        config = function()
+            -- Setup orgmode
+            require('orgmode').setup({
+                org_agenda_files = { '~/orgfiles/**/*', '~/school/**/*' },
+                org_default_notes_file = '~/orgfiles/refile.org',
+                org_todo_keywords = { 'TODO', 'NEXT', 'DOING', '|', 'DONE' },
+            })
+
+            -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+            -- add ~org~ to ignore_install
+            -- require('nvim-treesitter.configs').setup({
+            --   ensure_installed = 'all',
+            --   ignore_install = { 'org' },
+            -- })
+        end,
+    },
+    {
+        'akinsho/org-bullets.nvim',
+        ft = { 'org' },
+        config = function()
+            require('org-bullets').setup()
+        end
+    },
+}
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -30,6 +30,7 @@ return {
                 dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
                 dashboard.button("f", "󰍉  > Find file", "Telescope find_files<CR>"),
                 dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
+                dashboard.button("a", "A  > Settings", ":e ~/school/planning.org <CR>"),
                 dashboard.button("s", "  > Settings", ":e $MYVIMRC | :lcd %:p:h <CR>"),
                 dashboard.button(
                     "hs",

bb72dec9b5913d00a651a0b11e20ee0cc15b4cd5
seo <seowlfh@laposte.net>
2024-11-26 10:45:27 +0100

    formatter: add back

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -210,10 +210,10 @@ return {
                 sh = { "shellcheck" }
             },
 
-            -- format_on_save = {
-            --     timeout_ms = 500,
-            --     lsp_format = "fallback",
-            -- },
+            format_on_save = {
+                timeout_ms = 500,
+                lsp_format = "fallback",
+            },
         },
     },
     {

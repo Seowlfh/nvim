e3a927c260dd61af417e0ac8f52bd2e5361adcf0
seo <seowlfh@laposte.net>
2026-08-17 22:44:15 +0200

    feat: new cool options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,45 +1,62 @@
+local o = vim.opt
+
 -- Indentation
-vim.opt.tabstop = 8 -- Tab size
-vim.opt.shiftwidth = 4 -- Indent size
-vim.opt.expandtab = true -- Use spaces instead of tabs
+o.tabstop = 8 -- Tab size
+o.shiftwidth = 4 -- Indent size
+o.expandtab = true -- Use spaces instead of tabs
+o.smartindent = true
 
 -- Search
-vim.opt.incsearch = true -- Highlight matches while typing search query
-vim.opt.hlsearch = false -- Highlight previous search matches
-vim.opt.ignorecase = true -- Ignore case when searching
-vim.opt.smartcase = true -- Case sensitive search if the query contains upper case letters
+o.incsearch = true -- Highlight matches while typing search query
+o.hlsearch = false -- Highlight previous search matches
+o.ignorecase = true -- Ignore case when searching
+o.smartcase = true -- Case sensitive search if the query contains upper case letters
 
 -- Interface
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.colorcolumn = "80" -- Highlight column 80
-vim.opt.cursorline = true -- Highlight current line
-vim.opt.scrolloff = 10
-vim.opt.signcolumn = "yes"
-vim.opt.termguicolors = true
-vim.opt.showmode = false -- Display keystrokes
-vim.opt.showcmd = true -- Show command while typing
-vim.opt.laststatus = 2 -- Always show status bar
-vim.opt.wildmenu = true -- Better tab completion while typing commands
-vim.opt.scrolloff = 10 -- Always keep 10 line visiblie above or under the cursor
-vim.opt.splitbelow = true -- Split below the current window
-vim.opt.splitright = true -- Split right of the current window
-vim.opt.wrap = false
+o.number = true
+o.relativenumber = true
+o.colorcolumn = "80" -- Highlight column 80
+o.cursorline = true -- Highlight current line
+o.signcolumn = "yes"
+o.sidescrolloff = 8
+o.scrolloff = 10 -- Always keep 10 line visible above or under the cursor
+o.termguicolors = true
+o.showmode = false -- Display keystrokes
+o.showcmd = true -- Show command while typing
+o.laststatus = 2 -- Always show status bar
+o.wildmenu = true -- Better tab completion while typing commands
+o.splitbelow = true -- Split below the current window
+o.splitright = true -- Split right of the current window
+o.wrap = false
+o.pumheight = 10 -- Maximum height of pop-up menu for completion
+o.conceallevel = 2 -- Completely hide concealed text (text between **, etc.)
+
+-- Editing
+o.iskeyword:append("-") -- Treat dash as part of word
+o.path:append("**") -- include subdirectories in search
+o.virtualedit = "block"
+o.updatetime = 300
+o.timeoutlen = 300
+o.ttimeoutlen = 0
+o.shiftround = true
 
 -- File
-vim.opt.autoread = true -- Re-read file when it has been changed outside of vim
-vim.opt.autowrite = true -- Automatically write file on some commands (like :make, :next...)
-vim.opt.hidden = true -- Keeps buffers hidden when closed
-vim.opt.backup = false
-vim.opt.swapfile = false
+o.autoread = true -- Re-read file when it has been changed outside of vim
+o.autowrite = true -- Automatically write file on some commands (like :make, :next...)
+o.hidden = true -- Keeps buffers hidden when closed
+o.backup = false
+o.swapfile = false
+o.writebackup = false
+o.undofile = true
 
 -- Miscellaneaous
-vim.opt.list = true -- Enable listchars
-vim.opt.listchars = { tab = "➞ ", trail = "·" } -- Indicators for invisible characters
+o.list = true -- Enable listchars
+o.listchars = { tab = "➞ ", trail = "·" } -- Indicators for invisible characters
 
-vim.opt.clipboard = "unnamedplus"
+-- TODO: Trying to learn cliboard
+-- o.clipboard = "unnamedplus"
 
 -- Automatic linebreak
-vim.opt.textwidth = 80
-vim.opt.formatoptions:append({ "t" })
-vim.opt.linebreak = true
+o.textwidth = 80
+o.formatoptions:append({ "t" })
+o.linebreak = true
